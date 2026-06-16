87b718a288eb9761730ea67db13b5119ab774520
Adii-x <a.deokar2007@gmail.com>
2026-06-17 02:32:19 +0530

    Task Manager 50% Done

---
+++ DOM/Task Manager/script.js
@@ -0,0 +1,105 @@
+const createBtn = document.querySelector('#create');
+const formDiv = document.querySelector('.form');
+const closeBtn = document.querySelector('#close');
+const form = document.querySelector('form');
+const popUpCreate = document.querySelector('#taskCreate');
+const taskCont = document.querySelector('.taskCont');
+
+const taskArr = [];
+
+
+createBtn.addEventListener('click', () =>{
+
+    formDiv.style.display = "flex";
+
+})
+
+closeBtn.addEventListener('click', () =>{
+
+    formDiv.style.display = "none";
+
+})
+
+function comp(idx){
+
+    taskArr[idx].completed = true;
+    ui();
+
+}
+
+function dele(idx){
+    taskArr.splice(idx, 1);
+    ui();
+}
+
+let editTask = null;
+
+function ed(idx){
+    
+    const obj = taskArr[idx];
+
+    document.querySelector('#name').value = obj.taskName;
+    document.querySelector('#info').value = obj.taskInfo;
+    
+    editTask = idx;
+
+    formDiv.style.display = "flex";
+
+}
+
+const ui = () => {
+
+    taskCont.innerHTML = "";
+
+    taskArr.forEach((obj, idx) => {
+        taskCont.innerHTML += `
+            <div class="taskCard ${obj.completed ? "completed" : ""}">
+                <div class="text">
+                    <h2>${obj.taskName}</h2>
+                    <p>${obj.taskInfo}</p>
+                </div>
+                <div class="btns">
+                    <button onClick="ed(${idx})" id="edit">Edit</button>
+                    <button onClick="comp(${idx})" id="complete">Complete</button>
+                    <button onClick="dele(${idx})" id="delete">Delete</button>
+                </div>
+            </div>
+        `
+    })
+
+}
+
+form.addEventListener('submit', (e) =>{
+
+    e.preventDefault();
+
+    const taskName = e.target[0].value;
+    const taskInfo = e.target[1].value;
+
+    if(taskName.trim() === "" || taskInfo.trim() === ""){
+        alert("Please fill the fields");
+        return;
+    }
+
+    const obj = {
+        taskName,
+        taskInfo,
+        completed: false
+    };
+
+    
+    if(editTask === null){
+        taskArr.push(obj);
+    } else {
+        taskArr[editTask].taskName = taskName;
+        taskArr[editTask].taskInfo = taskInfo;
+
+        editTask = null;
+    }
+
+    ui();
+
+    form.reset();
+
+    formDiv.style.display = "none";
+})
